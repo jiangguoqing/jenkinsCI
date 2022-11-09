bd6ea34b6c2c70e142eff0747b1bb05fba76b715
Administrator <admin@example.com>
2022-11-09 11:07:32 +0800

    'jenkins'

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from littlej"))
+}
+
+// Add a showSnippet handler function.
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// Add a createSnippet handler function.
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
+
+func main() {
+	// Register the two new handler functions and corresponding URL patterns wi
+	// the servemux, in exactly the same way that we did before.
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+	log.Println("Starting server on :5000")
+	err := http.ListenAndServe(":5000", mux)
+	log.Fatal(err)
+}
